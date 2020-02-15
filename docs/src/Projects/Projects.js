65cf845866da9431eddd66a37589eb2b501eb965
giancarlokc <giancarlokc@gmail.com>
2020-02-15 14:06:11 -0800

    Simple page for projects.

--- docs/src/Projects/Projects.js
+++ docs/src/Projects/Projects.js
@@ -2,13 +2,22 @@ require('./projects.less')
 
 const React = require('react')
 const Page = require('../Page')
+const ProjectEntry = require('./ProjectEntry')
+const ProjectList = require('../Projects')
 
 class Projects extends React.PureComponent {
   render() {
     return (
       <Page>
         <div className='projects'>
-          projects
+          {ProjectList.map(project =>
+            <ProjectEntry
+              key={project.title}
+              title={project.title}
+              url={project.url}
+              date={project.date}
+            />
+          )}
         </div>
       </Page>
     )
